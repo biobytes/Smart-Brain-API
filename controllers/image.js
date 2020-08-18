0f807a8151a726c8106e35b768ce4c697b2859a6
Abz Aj <abbiajayi@gmail.com>
2020-08-18 23:08:18 +0100

    my API

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+                   apiKey: 'fd4e035c979341eb9bbc1820095aab1f'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+    .predict(
+         "a403429f2ddf4b49b307e318f00e528b",
+              req.body.input)
+    .then(data => {
+    	res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with APi'))
+}
+
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	 db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	 handleImage: handleImage,
+	 handleApiCall: handleApiCall
+}
